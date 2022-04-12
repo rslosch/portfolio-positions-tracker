7110280e187a630c1ba9613a0c629522324f08d3
Steven <slosch99@gmail.com>
2022-04-12 19:32:26 -0400

    Cleaned up code and installed MUI

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div >
+      <h1>Hello world</h1>
+      <Button variant="contained" >Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
